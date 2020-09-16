6c29b0efe137c18a0d80169bc9a4de17463c8276
mag103 <drabiukmagdalena@gmail.com>
2020-09-16 16:20:09 +0200

    Added search functionality to the index method on index page.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,19 +4,39 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if request.method == "POST":
+        query = request.POST.get('q')
+
+        if util.get_entry(query):
+            return entry(request, query)
+        else:
+            entries = util.list_entries()
+            search_results = []
+            for single_entry in entries:
+                if query.lower() in single_entry.lower():
+                    search_results.append(single_entry)
+            return render(request, "encyclopedia/search_results.html", {
+                "search_results": search_results
+            })
+
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
 
 def entry(request, title):
-    try:
+    if util.get_entry(title):
         content = markdown(util.get_entry(title))
         return render(request, "encyclopedia/entry.html", {
             "title": title, "content": content
         })
-    except TypeError:
+    else:
         return render(request, "encyclopedia/error.html", {
             "title": title
         })
+
+
+
+
 
